c34dabab581ea827a0a0aae965d550fbb6caef4c
Tiago Sales <tgoprince@gmail.com>
2022-03-25 14:33:54 +0100

    Add solution

---
+++ src/Land.java
@@ -0,0 +1,40 @@
+public class Land implements Sortable {
+  private int width = 0;
+  private int length = 0;
+
+  public Land(int width, int length) {
+    super();
+    setWidth(width);
+    setLength(length);
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public void setWidth(int width) {
+    this.width = width;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public void setLength(int length) {
+    this.length = length;
+  }
+
+  public int getArea() {
+    return this.getLength() * this.getWidth();
+  }
+
+  @Override
+  public int isBiggerThan(Sortable obj) {
+    Land otherLand = (Land) obj;
+    if (getArea() > otherLand.getArea())
+      return 1;
+    else
+      return 0;
+  }
+
+}
--- src/Runner.java
+++ src/Runner.java
@@ -1,22 +1,34 @@
 class Runner {
 
+  public static Sortable getBiggest(Sortable s1, Sortable s2, Sortable s3) {
+    Sortable biggest = s1;
+
+    if (s2.isBiggerThan(biggest) > 0)
+      biggest = s2;
+
+    if (s3.isBiggerThan(biggest) > 0)
+      biggest = s3;
+
+    return biggest;
+  }
+
   public static void main(String[] args) {
-    SavingsAccount sa = new SavingsAccount(10000);
-    System.out.println("account name: " + sa.getName());
-    System.out.println("initial amount: " + sa.getAmount());
+    Building b1 = new Building(20);
+    Building b2 = new Building(80);
+    Building b3 = new Building(60);
 
-    sa.deposit(5000);
-    System.out.println("new amount after deposit: " + sa.getAmount());
+    Building tallestBuilding = (Building) getBiggest(b1, b2, b3);
+    System.out.println("The tallest building is " +
+            tallestBuilding.getHeight() + "m high.");
 
-    CheckingAccount ca = new CheckingAccount(20000);
-    System.out.println("account name: " + ca.getName());
-    System.out.println("initial amount: " + ca.getAmount());
+    Land l1 = new Land(20, 30);
+    Land l2 = new Land(10, 5);
+    Land l3 = new Land(25, 25);
 
-    ca.deposit(6000);
-    System.out.println("new amount after deposit: " + ca.getAmount());
+    Land biggestLand = (Land) getBiggest(l1, l2, l3);
+    System.out.println("The biggest land is " + biggestLand.getArea() + "m2 (" +
+            biggestLand.getLength() + "x" + biggestLand.getWidth()+").");
 
-    ca.withdraw(3000);
-    System.out.println("new amount after withdrawal: " + ca.getAmount());
   }
 
 }
